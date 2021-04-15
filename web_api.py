0807ea8a643d4d409ffdf254e79c032a526af357
__gyulka__ <romagyuli-zade@yandex.ru>
2021-04-15 17:29:51 +0500

    доделал основную часть для апи бд

--- web_api.py
+++ web_api.py
@@ -60,5 +60,31 @@ def get_user():
         return jsonify(succes=False, error=error.__str__())
 
 
+@app.route('/api/set_menu')
+def set_menu():
+    try:
+        id = int(request.args['id'])
+        menu = int(request.args['menu'])
+        for user in session.query(User).filter(User.id == id):
+            user.menu = menu
+        session.commit()
+        return jsonify(succes=True)
+    except Exception as error:
+        jsonify(succes=False, error=error.__str__())
+
+
+@app.route('/api/add_score')
+def add_score():
+    try:
+        id = int(request.args['id'])
+        score = int(request.args['score'])
+        for user in session.query(User).filter(User.id == id):
+            user.score += score
+        session.commit()
+        return jsonify(succes=True)
+    except Exception as error:
+        jsonify(succes=False, error=error.__str__())
+
+
 if __name__ == '__main__':
     main()
