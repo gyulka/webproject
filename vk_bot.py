3de229fdacb53e2e167d8f67dff6f7d70bd6fad8
Sve4ka <sve4ka_one@mail.ru>
2021-04-21 19:10:05 +0500

    рома сказал что отправка денег законченная мысль

--- vk_bot.py
+++ vk_bot.py
@@ -17,7 +17,7 @@ token = "b2ac558da42dc23e631f34d3b5795ed6dbf8bd647001a703b8aa70f79bed894556abf4e
 bot = Bot(token=token)
 
 # -----клавиатуры
-keyboard_hello = Keyboard(one_time=False).add(Text("Начать", {"cmd": "start"})).get_json()
+keyboard_hello = Keyboard(one_time=True).add(Text("Начать", {"cmd": "start"})).get_json()
 
 keyboard_main = (Keyboard(one_time=False).add(Text("Купить", {"cmd": "aio"}))
                  .add(Text("Помочь", {"cmd": "aio"}))
@@ -50,13 +50,37 @@ keyboard_help = (Keyboard(one_time=False)
 # -----клавиатуры
 
 
+def shablon1():
+    a, b = 0, 1000
+    x = f"({random.randint(-b, b)} {random.choice(['*', '-', '+', '/'])} {random.randint(a, b)}) {random.choice(['*', '-', '+', '/'])} ({random.randint(-b, b)} {random.choice(['*', '-', '+', '/'])} {random.randint(a, b)})"
+    return x, eval(x)
+
+
+async def gen_primer(user_id, message):
+    shablon = random.choice([shablon1])
+    text, ans = shablon()
+    ans = round(ans, 2)
+    requests.post(db_server_api + 'set_helping', params={
+        'vk': True,
+        'id': user_id,
+        'helping': str(ans)
+    })
+    requests.post(db_server_api + 'set_menu', params={
+        'vk': True,
+        'id': user_id,
+        'menu': config.HELPING
+    })
+    print(ans)
+    await message.answer('Решите\n' + text + '\nчтобы прекратить, нажмите на любую кнопку')
+
+
 @bot.on.message(text="Привет")
 async def hi_handler(message: Message):
     users_info = await bot.api.users.get(message.from_id)
-    await message.answer("Привет, {}".format(users_info[0].first_name))
+    await message.answer("Привет, {}".format(users_info[0].first_name), keyboard=keyboard_hello)
 
 
-@bot.on.message(text=["/start <item>", "/start"])
+@bot.on.message(text=["/start <item>", "/start", 'Начать', '!start'])
 @bot.on.message(payload={"cmd": "start"})
 async def eat_handler(message: Message, item: Optional[str] = None):
     print(message)
@@ -90,8 +114,6 @@ async def eat_handler(message: Message, item: Optional[str] = None):
     elif txt == 'Купить':
         await message.answer(f"Выберите из списка:", keyboard=keyboard_buy)
     elif txt == 'Профиль':
-        user_info = await bot.api.users.get(message.from_id)
-        user_id = user_info[0].id
         text = f'''ник: {response['nick']}
         уникальный id: {user_id}_1
         баланс: {response['score']}
@@ -104,20 +126,109 @@ async def eat_handler(message: Message, item: Optional[str] = None):
 
         await message.answer(text, keyboard=keyboard_user)
     elif txt == 'Перевести':
-        user_info = await bot.api.users.get(message.from_id)
-        user_id = user_info[0].id
         requests.post(config.db_server_api + 'set_menu', params={
             'id': user_id,
             'vk': True,
-            'menu': config.TRANFER
+            'menu': config.TRANSFER
         })
-        await message.answer(f"Недоступно")
+        await message.answer('введите сумму и id куда перевести ввиде')
+        await message.answer('!go сумма id')
+    elif response['menu'] == config.TRANSFER:
+        summ, id = message.text.split()
+        response = requests.post(config.db_server_api + 'transfer', params={
+            'vk': True,
+            'id': user_id,
+            'score': summ,
+            'id_to': id
+        }).json()
+        if response['succes']:
+            await message.answer(f'успешно переведено {summ} {response["nick_to"]}')
+            requests.post(db_server_api + 'set_menu', params={
+                'vk': True,
+                'id': user_id,
+                'menu': 0
+            })
+            requests.post(db_server_api + 'set_menu', params={
+                'vk': True,
+                'id': user_id,
+                'menu': config.MAIN
+            })
+
+        else:
+            print(response['error'])
+    elif response['menu'] == config.HELPING:
+        if message.text == response['helping']:
+            requests.post(db_server_api + 'add_score', params={
+                'vk': True,
+                'id': user_id,
+                'score': 10
+            })
+            await message.answer('вам добавили 1')
+            await gen_primer(user_id, message)
+        else:
+            await message.answer('неверный ответ\nдля нового примера, нажмите на кнопку второй раз')
+    elif response['menu'] == config.CHANGE:
+        nick = message.text
+        requests.post(db_server_api + 'set_nick', params={
+            'vk': True,
+            'id': user_id,
+            'nick': nick
+        })
+        requests.post(db_server_api + 'set_menu', params={
+            'vk': True,
+            'id': user_id,
+            'menu': config.MAIN
+        })
+        await message.answer(f'ник успешно изменён на {message.text}', keyboard=keyboard_main)
+
+@bot.on.message(text='!go <summ> <id>')
+async def transfer(message: Message, summ: int, id: str):
+    user_info = await bot.api.users.get(message.from_id)
+    user_id = user_info[0].id
+    response = requests.post(config.db_server_api + 'transfer', params={
+        'vk': True,
+        'id': user_id,
+        'score': summ,
+        'id_to': id
+    }).json()
+    if response['succes']:
+        await message.answer(f'успешно переведено {summ} {response["nick_to"]}')
+        requests.post(db_server_api + 'set_menu', params={
+            'vk': True,
+            'id': user_id,
+            'menu': 0
+        })
+        requests.post(db_server_api + 'set_menu', params={
+            'vk': True,
+            'id': user_id,
+            'menu': config.MAIN
+        })
+
+    else:
+        print(response['error'])
+
 
 
 @bot.on.message(text=["/help <item>", "/help"])
 @bot.on.message(payload={"cmd": "help"})
 async def eat_handler(message: Message, item: Optional[str] = None):
-    await message.answer('введите сумму и id куда перевести через пробел', keyboard=keyboard_main)
+    user_info = await bot.api.users.get(message.from_id)
+    user_id = user_info[0].id
+    response = requests.get(config.db_server_api + 'get_user', params={
+        'id': user_id,
+        'vk': True
+    }).json()
+    if message.text == response['helping']:
+        requests.post(db_server_api + 'add_score', params={
+            'vk': True,
+            'id': user_id,
+            'score': 10
+        })
+        await message.answer('вам добавили 1')
+        await gen_primer(user_id, message)
+    else:
+        await message.answer('неверный ответ\nдля нового примера, нажмите на кнопку второй раз')
+
 
 
 @bot.on.message(text=["/puy <item>"])
@@ -136,11 +247,25 @@ async def eat_handler(message: Message, item: Optional[str] = None):
 
 @bot.on.message(text=["/user <item>"])
 @bot.on.message(payload={"cmd": 'user'})
-async def eat_handler(message: Message, item: Optional[str] = None):
+async def change(message: Message, item: Optional[str] = None):
     if item is None:
-        txt = message.text.split()
-        print(txt[0])
-    await message.answer(f"ну ок", keyboard=keyboard_main)
+        nick = message.text
+    else:
+        nick = item
+    user_info = await bot.api.users.get(message.from_id)
+    user_id = user_info[0].id
+    requests.post(db_server_api + 'set_nick', params={
+        'vk': True,
+        'id': user_id,
+        'nick': nick
+    })
+    requests.post(db_server_api + 'set_menu', params={
+        'vk': True,
+        'id': user_id,
+        'menu': config.MAIN
+    })
+    await message.answer(f'ник успешно изменён на {message.text}', keyboard=keyboard_main)
+
 
 
 @bot.on.message(text="Доброе утро")
